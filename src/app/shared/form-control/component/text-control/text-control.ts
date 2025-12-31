60a569e92bcefe272d8ef7a6dc1263afe6994b2e
Snehal Patel <patelsn@Waseel.com>
2025-12-31 10:03:40 +0530

    update in input/output with new signal syntax

--- src/app/shared/form-control/component/text-control/text-control.ts
+++ src/app/shared/form-control/component/text-control/text-control.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, input, Input, Output } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
@@ -29,32 +29,31 @@ import { CommonModule } from '@angular/common';
   styleUrl: './text-control.scss',
 })
 export class TextControl {
-  @Input() formControlModel!: FormControlModel;
-  @Input() form!: FormGroup;
-  @Input() class = '';
-  @Input() isDisable: boolean = false;
-  @Input() dynamicHeight: boolean = false;
-  @Input() autocomplete!: MatAutocomplete;
-  @Input() errorLength: number = 0;
-  // errorLength = input.required<number>();
-  @Output() inputChange = new EventEmitter<string>();
-  @Output() inputBlur = new EventEmitter<Event>();
-  @Output() enterInput = new EventEmitter<Event>();
-  @Output() iconClick = new EventEmitter<Event>();
+  formControlModel = input.required<FormControlModel>();
+  form = input.required<FormGroup>();
+  class = input<string>('');
+  isDisable = input<boolean>(false);
+  dynamicHeight = input<boolean>(false);
+  autocomplete = input<MatAutocomplete>();
+  errorLength = input<number>(0);
+  inputChange = output<string>();
+  inputBlur = output<void>();
+  enterInput = output<void>();
+  iconClick = output<Event>();
   // matIcons = MatIcons;
 
   constructor(public _validator: ValidatorService) {}
 
   onIconClick(event: Event) {
-    this.iconClick.emit();
+    this.iconClick.emit(event);
   }
 
   get errorText(): string {
     return (
       this._validator.getError(
-        this.form,
-        this.formControlModel.key,
-        this.formControlModel
+        this.form(),
+        this.formControlModel().key,
+        this.formControlModel()
       ) || ''
     );
   }
